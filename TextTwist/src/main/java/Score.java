d535057ec1409004a4f6296086e0a149448b3787
BrittLowell <brittany.lowell@revature.net>
2022-01-19 18:47:46 -0700

    Need to add comments, otherwise finished

--- TextTwist/src/main/java/Score.java
+++ TextTwist/src/main/java/Score.java
@@ -4,15 +4,7 @@ import java.util.HashMap;
 public class Score {
     Validation validation = new Validation();
 
-    //    Create a function that takes in an array of already-guessed words, the unscrambled 6-letter word and
-//    returns the total number of points the player scored in a particular round using the following rubric:
-//
-//            3-letter words are 1 pt
-//            4-letter words are 2 pts
-//            5-letter words are 3 pts
-//            6-letter words are 4 pts + 50 pt bonus (for unscrambling the word)
-//            Remember that invalid words (words that cannot be formed from the 6-letter unscrambled words)
-//            count as 0 pts.
+//TODO:Add comments to code and then submit coding challenge
     public int calcScore(ArrayList<String> guesses, String unscrambledWord){
         int len;
         int total = 0;
